69bafd41fc1c9b648f23216b7886406aaba9cda0
Thorgal08 <tomaspax@gmail.com>
2025-03-12 18:02:34 -0300

    comentario en app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// ESTO
+// rutas y componentes
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <Router>
